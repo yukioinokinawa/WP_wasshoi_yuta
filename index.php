b5f7c777742fe6fc46ced7c57a29e2c111a8ea7e
kota machino <65933978+airraid714@users.noreply.github.com>
2022-10-13 10:09:29 +0900

    header,footer,sidebarをテンプレートファイルに格納とphpで呼び出し

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php get_header(); ?>
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- ブレークポイントを作れるようにする -->
-        <?php wp_head(); ?>
-    </head>
-
-    <body>
-        <header class="l-header">
-            <a class="c-logo--header" href="#"><img src=<?php echo get_template_directory_uri().esc_url('/imgs/header-logo.jpg') ?> alt="わっしょいゆーたロゴ"></a>
-            <button class="c-button--hamburger js-hamburger">
-                <span>menuボタン</span>
-            </button>
-        </header>
         <main class="l-main">
             <div class="p-first-view">
                 <img src=<?php echo get_template_directory_uri().esc_url('/imgs/top-page-img.jpg"'); ?> alt="メイン写真">
@@ -23,26 +9,4 @@
                 </div>
             </div>
         </main>
-        <footer class="l-footer">
-            <p>©️わっしょいゆ〜た / 2022</p>
-        </footer>
-        <aside class="l-sidebar p-nav--sidebar">
-            <ul class="p-nav--sidebar__g-menu">
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">PROFILE</a></li>
-                <li><a href="#">HISTORY & WORKS</a></li>
-                <li><a href="#">CONTACT</a></li>
-            </ul>
-
-            <ul class="p-nav--sidebar__sns">
-                <li><a href="#"><img src=<?php echo get_template_directory_uri().esc_url('/imgs/facebook-icon.png') ?> alt="facebook"></a></li>
-                <li><a href="#"><img src=<?php echo get_template_directory_uri().esc_url('/imgs/twitter-icon.png') ?> alt="twitter"></a></li>
-                <li><a href="#"><img src=<?php echo get_template_directory_uri().esc_url('/imgs/instagram-icon.png') ?> alt="Instagram"></a></li>
-            </ul>
-    
-        </aside>
-        <div class="c-layer--sidebar"></div>
-        <?php wp_footer(); ?>
-    </body>
-
-</html>
+<?php get_footer(); ?>
