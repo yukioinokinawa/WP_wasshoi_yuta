23d256675a02d8e9e501921585e2550553dc8dec
kota machino <65933978+airraid714@users.noreply.github.com>
2022-10-14 14:42:04 +0900

    スライダーの実装と関連CSSの設定

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
         <main class="l-main">
             <div class="p-first-view">
-                <img src=<?php echo get_template_directory_uri().esc_url('/imgs/top-page-img.jpg"'); ?> alt="メイン写真">
+                <?php echo do_shortcode('[metaslider id="26"]'); ?>
                 <div class="c-top-title">
                     <h1>わっしょいゆ〜た</h1>
                     <p>OFFICIAL WEB SITE</p>
