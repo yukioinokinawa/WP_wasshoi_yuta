206c95b1c6b9147f08f62f4dce0b06e3366eb5ce
Taka <yukioinokinawa@gmail.com>
2022-10-19 18:24:55 +0900

    CSS途中まで

--- single.php
+++ single.php
@@ -1,7 +1,34 @@
 <?php get_header(); ?>
 
-    <main>
+<main class="l-main">
 
-    </main>
-    
+    <article class="p-single" <?php post_class(); ?>>
+        <article class="p-single__intro">
+            <h1 class="c-title--section"><?php the_title(); ?></h1>
+            <p class="c-date-and-category"><?php the_category(); ?></p>
+            <img class="p-card"> <?php
+                                    if (has_post_thumbnail()) :
+                                        the_post_thumbnail();
+                                    else :
+                                    ?><div class="no-thumbnail"></div><?php endif; ?>
+            </img>
+            <p class="c-date-and-category"><?php echo get_the_date(); ?></p>
+        </article>
+        <!--intro-->
+        <?php
+        if (have_posts()) :
+            while (have_posts()) :
+                the_post(); ?>
+
+                <?php the_content(); ?>
+            <?php endwhile;
+        else :
+            ?> <p>表示する記事がありません</p><?php
+                                    endif; ?>
+        <?php if (is_404()) : ?>
+            <?php get_template_part('component/404'); ?>
+        <?php endif; ?>
+    </article>
+</main>
+
 <?php get_footer(); ?>
