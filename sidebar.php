0d8ce57af7fc52a2169d4c6af52f26ff94d85527
kota machino <65933978+airraid714@users.noreply.github.com>
2022-11-04 13:12:52 +0900

    ツイッターアイコンの大きさ修正

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,7 @@
 
     <ul class="p-nav--sidebar__sns">
         <li><a href="https://www.facebook.com/wasshoiyuta"><img src=<?php echo get_template_directory_uri().esc_url('/imgs/facebook-icon.png') ?> alt="facebook"></a></li>
-        <li><a href="https://twitter.com/WasshoiYuta"><img src=<?php echo get_template_directory_uri().esc_url('/imgs/twitter-icon.png') ?> alt="twitter"></a></li>
+        <li><a href="https://twitter.com/WasshoiYuta" id="u-twitter-md"><img src=<?php echo get_template_directory_uri().esc_url('/imgs/twitter-icon.png') ?> alt="twitter"></a></li>
         <li><a href="https://www.instagram.com/wasshoiyuta_info/?hl=bg"><img src=<?php echo get_template_directory_uri().esc_url('/imgs/instagram-icon.png') ?> alt="Instagram"></a></li>
     </ul>
 
